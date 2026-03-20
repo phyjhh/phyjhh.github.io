1ab8e1d24b1f44be54d559203cd86b7cd26186f4
Huihui Jia <1719557265@qq.com>
2026-03-20 22:13:40 +0800

    fix: 更新所有使用 getPath 的文件，修复 next/prev 路由
    - PostDetails.astro: 更新导入和调用方式
    - posts/notes/publications slug 页面: 使用新的 getPath 签名
    - index.png.ts: 更新 OG 图片路径生成
    - rss.xml.ts: 更新 RSS 链接生成

--- src/pages/posts/[...slug]/index.png.ts
+++ src/pages/posts/[...slug]/index.png.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
 import { getCollection, type CollectionEntry } from "astro:content";
-import { getPath } from "@/utils/getPath";
+import getPath from "@/utils/getPath";
 import { generateOgImageForPost } from "@/utils/generateOgImages";
 import { SITE } from "@/config";
 
@@ -14,7 +14,7 @@ export async function getStaticPaths() {
   );
 
   return posts.map(post => ({
-    params: { slug: getPath(post.id, post.filePath, false) },
+    params: { slug: getPath(post, false) },
     props: post,
   }));
 }
